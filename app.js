70ec1a174237827c1b995f2f5a94e1b10702fd42
James Martinez <james-martinez@users.noreply.github.com>
2022-12-09 21:10:37 +0000

    need to filter

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const { Client, Events, GatewayIntentBits, messageLink } = require('discord.js');
+const https = require('https');
+const date = new Date();
 const { on } = require('events');
 const client = new Client({
     intents: [
@@ -13,9 +15,31 @@ client.on("ready", () => {
 client.on(Events.MessageCreate, msg => {
     if (msg.author.bot) return;
     switch (msg.content) {
-        case "!appt":
-            console.log(`Message from ${msg.author.username}: ${msg.content} , Channel: ${msg.channel.name}`);
-            msg.channel.send("Here's your appt!");
+        case "!":
+            const url = "https://idco.dmdc.osd.mil/idco/locator/site/170805/appnt/"
+                + date.getFullYear() + "-" + (date.getMonth() + 1);
+
+            const request = https.request(url, (response) => {
+                let data = '';
+                response.on('data', (chunk) => {
+                    data = data + chunk.toString();
+                });
+
+                response.on('end', () => {
+                    const body = JSON.parse(data);
+                    console.log(body);
+                    let _msgs = JSON.stringify(JSON.parse(data), null, 2);
+                    msg.channel.send('```json\n' + _msgs + '\n```');
+                });
+            })
+
+            request.on('error', (error) => {
+                console.log('An error', error);
+            });
+            request.end()
+
+
+
     }
 })
 client.login(process.env.DISCORD_TOKEN);
