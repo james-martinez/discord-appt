6e84867879eb21f7439be8e62d23e4ea00f7c475
James Martinez <james-martinez@users.noreply.github.com>
2022-12-09 12:14:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,11 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 client.on(Events.MessageCreate, msg => {
-    console.log(`Message from ${msg.author.username}: ${msg.content} , Channel: ${msg.channel.name}`);
     if (msg.author.bot) return;
     switch (msg.content) {
         case "!appt":
+            console.log(`Message from ${msg.author.username}: ${msg.content} , Channel: ${msg.channel.name}`);
             msg.channel.send("Here's your appt!");
-
     }
 })
 client.login(process.env.DISCORD_TOKEN);
