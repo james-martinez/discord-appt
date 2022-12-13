77be67301e2406d6fecb30b1412a0d172b025377
James Martinez <james-martinez@users.noreply.github.com>
2022-12-13 16:39:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -93,11 +93,11 @@ client.on(Events.MessageCreate, msg => {
             
         case "!appt!help":
             msg.channel.send(`Get current openings in for CAC appointments for the current month by site.
-            Commands to use in server:
-            !x -X Technologies, Inc.
-            !l - Lackand
-            !r - Randolph
-            !appt!help - Send this message`);
+Commands to use in server:
+!appt!help - Commands list
+!x - X Technologies, Inc.
+!l - Lackand
+!r - Randolph`);
             break;
     }
 })
