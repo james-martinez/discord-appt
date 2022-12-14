9df24238dee0cd1478fc6221d8fb6d9d712d5e84
James Martinez <james-martinez@users.noreply.github.com>
2022-12-14 15:35:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const client = new Client({
 });
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
+    msg.channel.send(`Get open CAC appointments for the current month by site.
+Commands to use in server:
+!appt!help - Commands list
+!x - X Technologies, Inc.
+!l - Lackand
+!r - Randolph`);
 })
 client.on(Events.MessageCreate, msg => {
     if (msg.author.bot) return;
